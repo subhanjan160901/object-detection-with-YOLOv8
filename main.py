642778b12034f72a2ad88b28d9efa6c85108024c
SUBHANJAN BASU <subhanjanbasu2001@gmail.com>
2024-03-06 12:53:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import cv2
+from ultralytics import YOLO
+import numpy as np
+
+
+cap = cv2.VideoCapture("/Users/subhanjanbasu/Desktop/assignment/myvideo.mp4")
+model = YOLO("yolov8m.pt")
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    results = model(frame, device="mps")
+    result = results[0]
+    bboxes = np.array(result.boxes.xyxy.cpu(), dtype="int")
+    classes = np.array(result.boxes.cls.cpu(), dtype="int")
+    for cls, bbox in zip(classes, bboxes):
+        (x, y, x2, y2) = bbox
+        cv2.rectangle(frame, (x, y), (x2, y2), (0, 0, 225), 2)
+        cv2.putText(frame, str(cls), (x, y - 5), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 225), 2)
+
+    cv2.imshow("Img", frame)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+cap.release()
+cv2.destroyAllWindows()
